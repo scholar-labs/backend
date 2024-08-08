eda75c50075fc5cf65695519d85faf1bbfdb75dd
Apoorv Saxena <apoorv.sxna@gmail.com>
2024-08-08 20:01:32 +0530

    fixed some errors
    Signed-off-by: Apoorv Saxena <apoorv.sxna@gmail.com>

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # here's how you can create a route
-app.route('/test')
+@app.route('/test')
 def say_hello():   
     return ('hello')
+
+if __name__ == '__main__':
+    app.run(debug=True)
